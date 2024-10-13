81bb624c191335ea8e07ab3dbda27d79fd5165c2
ovidiujordache <D00187472@student.dkit.ie>
2024-10-13 16:35:49 +0100

    removed further vars unused

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,7 +1,6 @@
  
 import React from 'react';
 
-import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
